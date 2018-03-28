7f2d9381a81ce75d5e83996ed7b9e847207b7086
sreekuttan <sreekuttanpurachery@gmail.com>
2018-03-28 15:10:34 +0530

    cb

--- Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,42 +1,29 @@
-package newproject;
+package qaclickacademy;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-//comment the above line and uncomment below line to use Chrome
-//import org.openqa.selenium.chrome.ChromeDriver;
-public class PG1 {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class SeleniumTest {
 
-    public static void main(String[] args) {
-        // declaration and instantiation of objects/variables
-    	System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		//comment the above 2 lines and uncomment below 2 lines to use Chrome
-		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-    	
-        String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
-
-        // launch Fire fox and direct it to the Base URL
-        driver.get(baseUrl);
-
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
-
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
-        }
-       
-        //close Fire fox
-        driver.close();
-       
-    }
-
+	//testng xml file from Maven
+	// how to execute all test cases from Test folder  - mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://facebook.com");
+        System.out.println(driver.getTitle());  
+        String s =driver.getCurrentUrl();
+        System.out.println(s);
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+	
+	
 }
