225eb860a8801a13dd1094b0fdf18011b19319f4
sreekuttan <sreekuttanpurachery@gmail.com>
2018-03-28 15:07:47 +0530

    changed

--- Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,29 +1,42 @@
-package qaclickacademy;
-
+package newproject;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//comment the above line and uncomment below line to use Chrome
+//import org.openqa.selenium.chrome.ChromeDriver;
+public class PG1 {
 
-public class SeleniumTest {
 
-	//testng xml file from Maven
-	// how to execute all test cases from Test folder  - mvn test
-	@Test
-	public void BrowserAUtomation()
-	{
-		System.out.println("BrowserAUtomation");	
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://facebook.com");
-        System.out.println(driver.getTitle());  
-        String s =driver.getCurrentUrl();
-        System.out.println(s);
-	}
-	@Test
-	public void elementsUi()
-	{
-		System.out.println("elementsUi");	
-	}
-	
-	
+    public static void main(String[] args) {
+        // declaration and instantiation of objects/variables
+    	System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+       
+    }
+
 }
