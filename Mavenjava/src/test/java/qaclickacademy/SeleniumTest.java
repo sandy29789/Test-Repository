e610ecc2804c1813d025c2f114cd72a8c9224314
sreekuttan <sreekuttanpurachery@gmail.com>
2018-04-03 16:21:33 +0530

    s

--- Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,7 +1,7 @@
 package qaclickacademy;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
