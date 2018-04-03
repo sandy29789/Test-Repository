3dd6d47eb4698806aaf806f65a7c4f5b97c2fd72
sreekuttan <sreekuttanpurachery@gmail.com>
2018-04-03 15:33:36 +0530

    s

--- Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,22 +1,27 @@
-import org.openqa.selenium.chrome.ChromeDriver;
+package qaclickacademy;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-
+	//testng xml file from Maven
+	// how to execute all test cases from Test folder  - mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
 		System.setProperty("webdriver.gecko.driver","/home/ubuntu/Documents/geckodriver");
 		WebDriver driver = new FirefoxDriver();
         Thread.sleep(3000);
 		driver.close();
-		
-		
-		
-		
 	}
-
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+	
+	
 }
